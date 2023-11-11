817a6e0c73e1da0705ee0be34dff0fc4e00de250
Joseph Acosta <allanacost.27@gmail.com>
2023-11-11 10:29:40 -0500

    React Forms: Estructura basica de navegacion entre formularios usando useState

--- forms_authentication/src/App.jsx
+++ forms_authentication/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import SingIn from "./components/SingIn";
+import SingUp from "./components/SingUp";
+import ForgotPassword from "./components/ForgotPassword";
+
+const App = () => {
+  const [step, setstep] = useState("singin");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="container">
+      {step === "singin" && <SingIn />}
+      {step === "singup" && <SingUp />}
+      {step === "forgot" && <ForgotPassword />}
+    </div>
+  );
+};
 
-export default App
+export default App;
