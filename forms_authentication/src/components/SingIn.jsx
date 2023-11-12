0da431499c02919b3882e8555bc2035ea47b5c6e
Joseph Acosta <allanacost.27@gmail.com>
2023-11-12 10:14:34 -0500

    React Forms: Creacion de componente Sing In

--- forms_authentication/src/components/SingIn.jsx
+++ forms_authentication/src/components/SingIn.jsx
@@ -1,5 +1,28 @@
 const SingIn = () => {
-  return <div>SingIn</div>;
+  return (
+    <main>
+      <h3>Sing In</h3>
+      <div className="card">
+        <form autoComplete="on">
+          <fieldset>
+            <label htmlFor="email">email</label>
+            <input type="email" id="email" autoFocus />
+            <p className="error">Valid email required</p>
+          </fieldset>
+          <fieldset>
+            <label htmlFor="password">password</label>
+            <input type="password" id="password" />
+            <p className="error">Password is required</p>
+          </fieldset>
+          <button type="submit">login</button>
+          <div className="forgot">Forgot Password</div>
+        </form>
+      </div>
+      <p>
+        Don&apos;t have an account? <span>Sing Up</span>
+      </p>
+    </main>
+  );
 };
 
 export default SingIn;
